109c7e5d93f30d2766989e3ae4c1790c0f02cbcb
hojat <hojat72elect@gmail.com>
2023-05-17 20:32:57 -0400

    added search endpoint of unsplash API

---
+++ src/components/Images.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Images = () => {
+    return (
+        <>
+            <h1 className="text-center mt-6 underline text-2xl">Results for cats</h1>
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10 ml-2 mr-2">
+                <h1>Image 1</h1>
+                <h1>Image 2</h1>
+                <h1>Image 3</h1>
+                <h1>Image 4</h1>
+                <h1>Image 5</h1>
+                <h1>Image 6</h1>
+                <h1>Image 7</h1>
+                <h1>Image 8</h1>
+                <h1>Image 9</h1>
+            </div>
+        </>
+    )
+}
+export default Images
