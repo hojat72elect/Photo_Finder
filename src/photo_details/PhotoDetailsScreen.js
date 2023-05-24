cffede3f6b3087d197594305dd815ea46b46acfb
hojat <hojat72elect@gmail.com>
2023-05-24 13:16:36 -0400

    Passed data between router destinations

--- src/photo_details/PhotoDetailsScreen.js
+++ src/photo_details/PhotoDetailsScreen.js
@@ -4,8 +4,6 @@ function PhotoDetailsScreen(props) {
 
     const imageUrl = props.location.state.urls.regular;
 
-    console.warn(`the current page:
-${imageUrl}\nand nothing else.\n`);
     return (
         <h1>{imageUrl}</h1>
     )
