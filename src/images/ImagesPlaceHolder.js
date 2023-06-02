be88256f78a639ab96c9ccd9b0ac0490c2c3e9bf
hojat <hojat72elect@gmail.com>
2023-06-02 12:03:51 -0400

    docs

--- src/images/ImagesPlaceHolder.js
+++ src/images/ImagesPlaceHolder.js
@@ -3,9 +3,16 @@ import React from 'react'
 /**
  * While pictures are being loaded from server, this place-holder will be shown for the provided number of pictures.
  */
+/**
+ *
+ * @param {number} item number of placeholders we're going to show.
+ * @returns {unknown[]}
+ * @constructor
+ */
 const ImagesPlaceHolder = ({item}) => {
-    return [...Array(item).keys()].map(() => (
-        <div className="animate-pulse">
+
+    return [...Array(item).keys()].map((key) => (
+        <div key={key} className="animate-pulse">
             <div className="bg-gray-300 rounded-lg h-72">
 
             </div>
