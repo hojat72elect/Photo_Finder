ba24736a86d13952d25e5bbaf762641fadcc2163
hojat <hojat72elect@gmail.com>
2023-06-08 20:47:47 -0400

    Added warm up stuff to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {createContext, useState} from 'react';
+import UnsplashService from "./data/UnsplashService";
+import {UNSPLASH_ACCESS_KEY} from "./.secrets/Keys";
+
+// Create Context
+export const ImageContext = createContext(null);
+
 
 function App() {
+
+    const [searchImage, setSearchImage] = useState('');
+    const {
+        pageCount,
+        response,
+        isLoading,
+        error,
+        fetchData
+    } = UnsplashService(`search/photos?page=1&query=montreal&client_id=${UNSPLASH_ACCESS_KEY}`);
+
+    const value = {
+        response,
+        isLoading,
+        error,
+        fetchData,
+        searchImage,
+        setSearchImage
+    }
+
     return (
         <div className="App">
             <header className="App-header">
