150197bb98632820c630c808e28916ffbb6e531d
hojat <hojat72elect@gmail.com>
2023-06-10 13:49:07 -0400

    ScreenHeader.js -> TS

--- src/search/ScreenHeader.tsx
+++ src/search/ScreenHeader.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
+interface Props {
+    children: JSX.Element;
+}
+
 /**
  * The header part of the screen (contains the [SearchField]).
  * It depicts a header title and also shows the [children] below that title.
  *
  */
-const ScreenHeader = ({children}) => {
+const ScreenHeader: React.FC<Props> = ({children,}) => {
     return (
         <div className="bg-gray-900 flex items-center py-10">
             <div className="max-w-md mx-auto w-full">
