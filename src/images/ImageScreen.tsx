71f40cd71f614d9e015635b39b34b72526e05387
hojat <hojat72elect@gmail.com>
2023-06-10 22:42:23 -0400

    ImageScreen.js -> TS

--- src/images/ImageScreen.tsx
+++ src/images/ImageScreen.tsx
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react'
 import {ImageContext} from "../App";
 import ImageButton from "./ImageButton";
+import {ApiPhoto} from "../data/api/unsplash/ApiSearchResult";
 
 /**
  * The page for showing requested images.
@@ -10,14 +11,15 @@ import ImageButton from "./ImageButton";
  */
 const ImageScreen = () => {
 
+    // @ts-ignore
     const {response, searchImage} = useContext(ImageContext);
 
     return (
         <>
             <h1 className="text-center mt-6 underline text-2xl">Results for {searchImage || 'Cats'}</h1>
             <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10 max-w-7xl mx-auto px-4">
-                {response.map((data, key) => <ImageButton key={key}
-                                                          data={data}/>)}
+                {response.map((data: ApiPhoto, key: number) => <ImageButton key={key}
+                                                                            data={data}/>)}
             </div>
         </>
     )
