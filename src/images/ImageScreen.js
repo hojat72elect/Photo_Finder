7611d1a47e83398e214bc46df722bb1b908f4cfe
hojat <hojat72elect@gmail.com>
2023-06-10 22:22:50 -0400

    Removed ImagesPlaceHolder because it was poorly done.

--- src/images/ImageScreen.js
+++ src/images/ImageScreen.js
@@ -1,7 +1,6 @@
 import React, {useContext} from 'react'
 import {ImageContext} from "../App";
 import ImageButton from "./ImageButton";
-import ImagesPlaceHolder from "./ImagesPlaceHolder"
 
 /**
  * The page for showing requested images.
@@ -11,14 +10,14 @@ import ImagesPlaceHolder from "./ImagesPlaceHolder"
  */
 const ImageScreen = () => {
 
-    const {response, isLoading, searchImage} = useContext(ImageContext);
+    const {response, searchImage} = useContext(ImageContext);
 
     return (
         <>
             <h1 className="text-center mt-6 underline text-2xl">Results for {searchImage || 'Cats'}</h1>
             <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10 max-w-7xl mx-auto px-4">
-                {isLoading ? <ImagesPlaceHolder item={10}/> : response.map((data, key) => <ImageButton key={key}
-                                                                                                       data={data}/>)}
+                {response.map((data, key) => <ImageButton key={key}
+                                                          data={data}/>)}
             </div>
         </>
     )
